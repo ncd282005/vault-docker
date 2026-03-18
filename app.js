2c0c0b52510f515abc46e4956e9c64a5983b8871
ncd282005 <ncd635473@gmail.com>
2026-03-18 14:11:09 +0530

    Refactor health check route message
    Updated health check route message for clarity.

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use("/api/v1/webhook/shopify", webhookShopifyRoutes);
 app.get("/", (req, res) => {
   res.status(200).json({
     success: true,
-    message: " API IS RUNNING "
+    message: " vault with docker and git "
   });
 });
 
